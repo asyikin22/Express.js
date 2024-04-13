f8cdb0fc5a791a00966e4db08ae57caf79b1c36e
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-04-13 20:53:07 +0800

    put + delete method

--- 08-methods.js
+++ 08-methods.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({extended: false}))
 //parse JSON
 app.use(express.json())
 
+//GET METHOD
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: people});
 })
@@ -36,6 +37,42 @@ app.post('/login', (req, res) => {
 
    res.status(401).send('Please Provide Credentials')
 })
+
+//PUT METHOD
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const{ name } = req.body
+    
+    const person = people.find((person) => person.id === Number(id))
+        if(!person) {
+            return res
+            .status(404)
+            .json({ success: false, msg: `no person with id ${id}` })
+        }
+
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({ success: true, data: newPeople })
+})
+
+//DELETE METHOD
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if (!person) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `no person with id ${req.params.id}` })
+    }
+    const newPeople = people.filter(
+      (person) => person.id !== Number(req.params.id)
+    )
+    return res.status(200).json({ success: true, data: newPeople })
+  })
+  
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')
