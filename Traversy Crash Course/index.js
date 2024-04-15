8de629f11d99e626e29ab16740eb3a163ccf8a7a
asyikin22 <148519441+asyikin22@users.noreply.github.com>
2024-04-15 22:15:19 +0800

    CRUD - UPDATE & DELETE

--- Traversy Crash Course/index.js
+++ Traversy Crash Course/index.js
@@ -9,11 +9,17 @@ const app = express();
 
 // app.use(logger)
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 //set static folder
 app.use(express.static('../public'))
 
+//ROUTER MIDDLEWARE MOUNTING /  USERS API ROUTE
 app.use('/api/users', require('./route-users'));
 
 const PORT = process.env.PORT || 5005;
 
 app.listen (PORT, () => console.log(`Server started on port ${PORT}`));
+
--- Traversy Crash Course/route-users.js
+++ Traversy Crash Course/route-users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const uuid = require('uuid')
 const router = express.Router();
 const users = require('./users');
 
@@ -19,6 +20,62 @@ router.get('/:id', (req, res) => {
         }
     });
 
+//CREATES A USER + ADD TO ARRAY
+router.post('/', (req, res) => {
+    const newUser = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    if(!newUser.name || !newUser.email) {
+       return res.status(400).json( { msg: 'Please include a name and email'})
+    }
+
+    users.push(newUser);
+    res.json(users);
+
+});
+
+//UPDATE MEMBER
+router.put('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
+
+    if (found) {
+        const updUser = req.body;
+        users.forEach(user => {
+            if(user.id === parseInt(req.params.id)) {
+                user.name = updUser.name ? updUser.name : user.name;
+                user.email = updUser.email ? updUser.email : user.email;
+
+                res.json( {msg: 'User updated', user})
+            }
+        });
+        
+        } else {
+            res.status(400).json({ msg: `No user with the ID of ${req.params.id}`})
+        }
+    });
+
+//DELETE USER
+router.delete('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json( { msg: 'User deleted', 
+        users: users.filter(user => user.id !== parseInt(req.params.id))
+    });
+
+        } else {
+            res.status(400).json({ msg: `No user with the ID of ${req.params.id}`})
+        }
+    });
+
 module.exports = router;
+
+
+
+
 
 
